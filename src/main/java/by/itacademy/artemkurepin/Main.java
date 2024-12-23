11f42bc32e96504edc2a59c107acb46aeea85b7b
Артём <artem.kurepin999@gmail.com>
2024-12-23 20:55:06 +0300

    Cities

---
+++ src/main/java/by/itacademy/artemkurepin/Main.java
@@ -0,0 +1,15 @@
+package by.itacademy.artemkurepin;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> cities = new ArrayList<>();
+        cities.add("Moscow");
+        cities.add("Minsk");
+        cities.add("New York");
+        cities.add("Brest");
+        cities.add("Ghomel");
+        System.out.println(cities);
+    }
+}
